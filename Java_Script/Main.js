1c898722885ea17d70812d0d5b27b526e083f0f2
geekyom07 <om17092004@gmail.com>
2024-04-15 22:44:07 +0530

    first commit

---
+++ Java_Script/Main.js
@@ -0,0 +1,15 @@
+function KitchenClick() {
+    console.log("GO to kitchen!")
+}
+function HallClick() {
+    console.log("GO to Hall!")
+}
+function BedroomClick() {
+    console.log("GO to Bedroom!")
+}
+function DinningClick() {
+    console.log("GO to Dinning Room!")
+}
+function BathroomClick() {
+    console.log("GO to Bathroom!")
+}
